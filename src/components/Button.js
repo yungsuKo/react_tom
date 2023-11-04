a84cfdedb01eecb51174225d46fc51a92981f491
Tom Ko <yungsu2391@gmail.com>
2023-11-04 12:20:04 +0900

    props component

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+const Button = (props) => {
+  return (
+    <div>
+      <button>{props.text}</button>
+    </div>
+  );
+};
+
+export default Button;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,12 @@
+import Button from '../components/Button';
+
 const Home = () => {
-  return <div>This is Home</div>;
+  return (
+    <div>
+      This is Home
+      <Button text="안녕" />
+    </div>
+  );
 };
 
 export default Home;

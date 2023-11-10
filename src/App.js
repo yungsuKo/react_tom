3664474f911b96ef91299f15c98054bcd8109065
Tom Ko <yungsu2391@gmail.com>
2023-11-10 23:43:39 +0900

    app / header edit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Header from './layouts/Header';
 import Footer from './layouts/Footer';
 import Seller from './pages/Seller';
-import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Helmet></Helmet>
         <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -2,19 +2,6 @@ import { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 
 const Header = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-
-    script.src = 'https://use.typekit.net/foobar.js';
-    script.async = true;
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <div>
       <Navbar />

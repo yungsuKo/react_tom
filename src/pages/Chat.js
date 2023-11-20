512bb572c3a2dcdb5be58d23e24e21b518e457cf
Tom Ko <yungsu2391@gmail.com>
2023-11-20 20:50:45 +0900

    chat room 구현

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -8,29 +8,46 @@ const socket = io(ENDPOINT);
 const Chat = () => {
   const [message, setMessage] = useState('');
   const [history, setHistory] = useState([]);
+  const [nickname, setNickname] = useState('');
+  const [room, setRoom] = useState('');
 
   useEffect(() => {
-    socket.on('ServerToClient', (args) => {
-      console.log('args', Object.values(args)[0]);
-      setHistory([...history, Object.values(args)[0]]);
-      history.map((chat, idx) => console.log('chat', idx, chat));
+    socket.on(room, (args) => {
+      setHistory([...history, args]);
       setMessage('');
     });
+    console.log(room);
+    socket.on('comeOn' + room, (comeOn) => {
+      setHistory([...history, comeOn]);
+    });
+
     return () => {
       socket.emit('disconnected');
       socket.off();
     };
-  }, [history]);
+  }, [history, room]);
 
   const onChat = (e) => {
     setMessage(e.target.value);
   };
+  const onNickname = (e) => {
+    setNickname(e.target.value);
+  };
+  const onRoom = (e) => {
+    setRoom(e.target.value);
+  };
   const onSend = (e) => {
-    socket.emit('ClientToServer', { message });
+    socket.emit('send', { room, nickname, message });
+  };
+  const onJoin = () => {
+    socket.emit('JoinRoom', { room, nickname });
   };
 
   return (
     <>
+      <input placeholder="닉네임을 입력하세요" onChange={onNickname} />
+      <input placeholder="room 번호를 입력하세요" onChange={onRoom} />
+      <button onClick={onJoin}>joinroom</button>
       <ul className="chat-history">
         <Messages histories={history} />
       </ul>

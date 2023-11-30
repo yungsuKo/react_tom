ee8546aa4a7c67ea42efa0715f75d975464c9ab7
Tom Ko <yungsu2391@gmail.com>
2023-11-30 22:17:30 +0900

    tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Products from './pages/Products';
-import Header from './layouts/Header';
-import Footer from './layouts/Footer';
+import './index.css';
 import Seller from './pages/Seller';
 import Chat from './pages/Chat';
 
@@ -11,14 +10,12 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/products" element={<Products />}></Route>
           <Route path="/seller" element={<Seller />}></Route>
           <Route path="/chat" element={<Chat />}></Route>
         </Routes>
-        <Footer />
       </BrowserRouter>
     </div>
   );

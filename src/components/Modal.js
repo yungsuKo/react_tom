4fb2ed6327c38fadc892aa95bff758b6230958a2
Tom Ko <yungsu2391@gmail.com>
2023-12-08 21:49:01 +0900

    modal background close

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,13 +4,12 @@ import './Modal.css';
 const Modal = ({ text, onClick }) => {
   return (
     <>
-      <div className="modal_backgroud">
-        <div className="modal_container">
-          <div className="modal_header"></div>
-          <div className="modal_content">{text}</div>
-          <div className="modal_footer">
-            <Button onClick={onClick}>dd</Button>
-          </div>
+      <div className="modal_backgroud" onClick={onClick}></div>
+      <div className="modal_container">
+        <div className="modal_header"></div>
+        <div className="modal_content">{text}</div>
+        <div className="modal_footer">
+          <Button onClick={onClick}>dd</Button>
         </div>
       </div>
     </>
